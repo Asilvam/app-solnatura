27e362d9228c428679408caef1bb10f9a8004a30
unknown <alejandro.silva.m@gmail.com>
2020-07-26 02:32:37 -0400

    mejoras de indentacion, mejoras de .env

--- src/database.js
+++ src/database.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
-// const URI ='mongodb+srv://root:admin@cluster0-jim6x.mongodb.net/test?retryWrites=true&w=majority';
-const URI ='mongodb://root:admin@cluster0-shard-00-00-jim6x.mongodb.net:27017,cluster0-shard-00-01-jim6x.mongodb.net:27017,cluster0-shard-00-02-jim6x.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-
-mongoose.connect(URI, {
+const mongoose = require("mongoose");
+// console.log(process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('Db is connected'))
-    .catch(err => console.log(err));
+    useUnifiedTopology: true,
+  })
+  .then((db) => console.log("Db is connected"))
+  .catch((err) => console.log(err));
