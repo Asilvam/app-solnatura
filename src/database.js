cc24c10596cd7f802c54db40c3bf4eaeeeb8b23b
alejandrosilvamonsalve <alejmsilvam@falabella.cl>
2024-09-29 23:52:48 -0300

    fix warnings

--- src/database.js
+++ src/database.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 // const URI ='mongodb+srv://root:admin@cluster0-jim6x.mongodb.net/test?retryWrites=true&w=majority';
 const URI =
   "mongodb://root:admin@cluster0-shard-00-00-jim6x.mongodb.net:27017,cluster0-shard-00-01-jim6x.mongodb.net:27017,cluster0-shard-00-02-jim6x.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-mongoose
-  .connect(URI, {
+mongoose.set('strictQuery', true);
+mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
